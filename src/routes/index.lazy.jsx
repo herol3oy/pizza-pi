c783f0ce825b0f4d694b50762781a2b0c2666573
Hamed <hamedsedighi@gmail.com>
2024-11-03 11:27:38 +0100

    add err boundary

--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -1,8 +1,8 @@
-import { createLazyFileRoute, Link } from "@tanstack/react-router";
+import { createLazyFileRoute, Link } from "@tanstack/react-router"
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
-});
+})
 
 function Index() {
   return (
@@ -18,7 +18,10 @@ function Index() {
         <li>
           <Link to="/past">Past Orders</Link>
         </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
       </ul>
     </div>
-  );
+  )
 }
