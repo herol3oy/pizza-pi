c57dbd1668a3f909a7fded9069e301d04864bddf
Hamed <hamedsedighi@gmail.com>
2024-11-04 20:18:01 +0100

    add vitest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   server: {
@@ -16,4 +16,7 @@ export default defineConfig({
     },
   },
   plugins: [react(), TanStackRouterVite()],
+  test: {
+    environment: "happy-dom",
+  },
 })
